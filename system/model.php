a77329936061b8522f8e523b7245612800caff5c
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-02-24 07:47:32 +0200

    Added timestamp function on model.php used as '2 weeks ago'

--- system/model.php
+++ system/model.php
@@ -13,4 +13,25 @@ class Model {
     );
   }
 
+  // 2 weeks ago
+  public function timestamp($input){
+    $input = explode(" ", $input);
+    $time = time();
+    $cantity = $input[0];
+    $type = $input[1];
+    switch ($type) {
+      case "weeks":
+        return $time - ($cantity*604800);
+        break;
+      case "week":
+        return $time - ($cantity*604800);
+        break;
+      case "days":
+        return $time - ($cantity*86400);
+        break;
+      case "day":
+        return $time - ($cantity*86400);
+        break;
+    }
+  }
 }
