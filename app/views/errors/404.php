99515e186a6b995511c5fc3ee4c76a4b52045f10
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-03-08 01:36:28 +0200

    Updated error page

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -1,4 +1,3 @@
-<?php include(VIEW. 'common/header.php'); ?>
-    <h1>404 Error</h1>
-    <p>Looks like this page doesn't exist.</p>
-<?php include(VIEW. 'common/footer.php'); ?>
+<h1>404 Error</h1>
+<p>Looks like this page doesn't exist.</p>
+
