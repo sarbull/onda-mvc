3370cfb8e156a689dca0e56f93b3d4a4c831b943
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-08-22 01:41:38 +0300

    Removed old mysql structure

--- app/views/errors/404.php
+++
@@ -1,2 +0,0 @@
-    <h1>404 Page not found</h1>
-    <p><span style="position:relative;bottom:1px;">&laquo;</span> <a href="<?=$this->router->generate("dashboard")?>">Back to Dashboard</a></p>
--- public/js/404.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  $("#content").attr({"style":"margin-top:200px;width:300px;text-align:center;padding:50px 0px 50px 0px;"});
-});
