aa3787093db8e69f05421d26f14c4f49656d07ed
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-10-02 03:52:48 +0300

    Moved title up

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -2,10 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <title><?=@$page_title ? $page_title : "Welcome"; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="">
     <meta name="keywords" content="">
-    <title><?=@$page_title ? $page_title : "Welcome"; ?></title>
 <?php foreach (@$this->css as $css) { ?>
     <link rel="stylesheet" href="<?=$css?>" type="text/css" media="screen">
 <?php } ?>
