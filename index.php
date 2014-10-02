d36d11be026e52cede496d0039770c1be1170782
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-10-02 05:16:41 +0300

    Conditioned the config create step

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ session_start();
 
 define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
 
-require(ROOT_DIR . 'config/config.php');
+$config_file = ROOT_DIR . 'config/config.php';
+
+if(file_exists($config_file)){
+  require($config_file);
+} else {
+  die("Make sure that you create the /config/config.php file.");
+}
 
 $application = new Application();
 
